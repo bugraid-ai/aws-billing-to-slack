5cd8525e4db4583e6a8e3114b1c3266ea0c15283
vaibhav-P99 <147043180+vaibhav-P99@users.noreply.github.com>
2024-09-24 15:51:22 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -210,12 +210,12 @@ def report_cost(group_by: str = "SERVICE", length: int = 5, cost_aggregation: st
         else:
             emoji = ":warning:"
 
-        summary = (f"{emoji} Yesterday's cost for {account_name} ${total_costs[-1]:,.2f} @Rajat "
+        summary = (f"{emoji} Yesterday's cost for {account_name} ${total_costs[-1]:,.2f} @Vaibhav Kadam"
                    f"is {relative_to_budget:.2f}% of credit budget "
                    f"${allowed_credits_per_day:,.2f} for the day."
                   )
     else:
-        summary = f"Yesterday's cost for account {account_name} was ${total_costs[-1]:,.2f} @Rajat"
+        summary = f"Yesterday's cost for account {account_name} was ${total_costs[-1]:,.2f} @Vaibhav Kadam"
 
     return summary, buffer, cost_per_day_by_service
 
